31abd9c81b752e327d46998ecfdaba7a081600ed
Von B. <lorenzo.casbarra@gmail.com>
2026-01-20 17:26:01 +0100

    feat: access ftp.enseml.org with lftp

--- lib/bash/Ftp2Ensembl.sh
+++ lib/bash/Ftp2Ensembl.sh
@@ -8,3 +8,7 @@ else
    fasta_path="release-"$ensversion"/fasta/"
 fi
 mkdir -p $ericscriptfolder/lib/data/_resources/
+
+lftp -c "open ftp.ensembl.org; cd pub/; ls -1 > $ericscriptfolder/lib/data/_resources/.ftplist0"
+lftp -c "open ftp.ensembl.org; cd pub/${fasta_path}; ls -1R > $ericscriptfolder/lib/data/_resources/.ftplist1"
+
