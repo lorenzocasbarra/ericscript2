c48e431d6daf37b94ea7bbf1d66813212193237d
Von B. <lorenzo.casbarra@gmail.com>
2026-01-20 17:24:31 +0100

    feat: lib/data/_resources created if missing

--- lib/bash/Ftp2Ensembl.sh
+++ lib/bash/Ftp2Ensembl.sh
@@ -7,11 +7,4 @@ if [ $ensversion -eq 0 ]; then
 else 
    fasta_path="release-"$ensversion"/fasta/"
 fi
-ftp -vi ftp.ensembl.org >> ~/.ericscript.log 2>&1 <<ftpj
-bin
-cd pub/
-ls -1 $ericscriptfolder/lib/data/_resources/.ftplist0
-cd ${fasta_path}
-ls -1R $ericscriptfolder/lib/data/_resources/.ftplist1
-quit
-ftpj
+mkdir -p $ericscriptfolder/lib/data/_resources/
